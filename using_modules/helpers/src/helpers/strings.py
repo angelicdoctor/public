a35ff8df2638f369bf4ff11e16cbbcfa590d4c8e
angelicdoctor <62401575+angelicdoctor@users.noreply.github.com>
2020-05-04 11:58:56 -0500

    Lab Work
    Python training files

---
+++ using_modules/helpers/src/helpers/strings.py
@@ -0,0 +1,12 @@
+def extract_upper(phrase):
+    """
+    extract_upper takes a string and return a list containing
+    only the uppercase characters from the string.
+
+    >>> extract_upper("Hello There, BOB")
+    ['H', 'T', 'B', 'O', 'B']
+    """
+    return list(filter(str.isupper, phrase))
+    
+def extract_lower(phrase):
+    return list(filter(str.islower, phrase))
